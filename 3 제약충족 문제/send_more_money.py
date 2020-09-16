933dcd7bf55589a28882592dc7a551210bb0e8dc
sihyun <gthpgth@gmail.com>
2020-09-16 23:53:58 +0900

    send more money

---
+++ 3 제약충족 문제/send_more_money.py
@@ -0,0 +1,44 @@
+from csp import CSP, Constraint
+from typing import Dict, List, Optional
+
+
+class SendMoreMoneyConstraint(Constraint[str, int]):
+    def __init__(self, letters: List[str]) -> None:
+        super().__init__(letters)
+        self.letters: Lost[str] = letters
+
+    def satisfied(self, assignment: Dict[str, int]) -> bool:
+        # 중복 값이 있다면 이 할당은 답이 아니다
+        if len(set(assignment.values())) < len(assignment):
+            return False
+
+        # 모든 변수에 숫자를 할당해서 계싼이 맞는지 확인한다
+        if len(assignment) == len(self.letters):
+            s: int = assignment["S"]
+            e: int = assignment["E"]
+            n: int = assignment["N"]
+            d: int = assignment["D"]
+            m: int = assignment["M"]
+            o: int = assignment["O"]
+            r: int = assignment["R"]
+            y: int = assignment["Y"]
+            send: int = s * 1000 + e * 100 + n * 10 + d
+            more: int = m * 1000 + o * 100 + r * 10 + e
+            money: int = m * 10000 + o * 1000 + n * 100 + e * 10 + y
+            return send + more == money
+        return True
+
+
+if __name__ == "__main__":
+    letters: List[str] = ["S", "E", "N", "D", "M", "O", "R", "Y"]
+    possible_digits: Dict[str, List[int]] = {}
+    for letter in letters:
+        possible_digits[letter] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    possible_digits["M"] = [1]  # 답은 0으로 시작하지 않는다
+    csp: CSP[str, int] = CSP(letters, possible_digits)
+    csp.add_constraint(SendMoreMoneyConstraint(letters))
+    solution: Optional[Dict[str, int]] = csp.backtracking_search()
+    if solution is None:
+        print("답을 찾을 수 없습니다")
+    else:
+        print(solution)
